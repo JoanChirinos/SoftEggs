ba2f54413ab436e2aeb518f8fda089ac5333c5ad
tfabiha <tfabiha@stuy.edu>
2018-10-21 16:50:07 -0400

    fixed error in discobandit.db

--- util/access_data.py
+++ util/access_data.py
@@ -1,40 +1,66 @@
 import sqlite3
 
-DB_FILE="data/discoeggs.db"
-
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()
-
 def sign_up(user, pwd):
+    DB_FILE="data/discoeggs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+
+    #still have to make sure that we cannot create multiple of the same users
+
     #we gotta find out how many rows there are already
     #see what happens now using that
     command = "SELECT COUNT(*) FROM login"
+
     c.execute(command)
     rows = c.fetchone()[0]
     #print(rows)
     params = (user, pwd, rows+1)
     c.execute("INSERT INTO login VALUES (?,?,?)", params)
+
+    db.commit() #save changes
+    db.close()  #close database
+
     return True
 
-sign_up("Scriptor","nah")
+#sign_up("Scriptor","nah")
 
 def login(user,pwd):
+    DB_FILE="data/discoeggs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+
     command = "SELECT password FROM login WHERE username = \'{}\'".format(user)
     c.execute(command)
-    password = c.fetchone()[0]
+    password = c.fetchone()
     #print(password)
-    if pwd == password:
-        return True
-    return False
+
+    db.commit() #save changes
+    db.close()  #close database
+
+    #print(password);
+    if password == None or password[0] != pwd:
+        return False
+
+    return True
+
 '''
+login('bob', 'bobby')
+
 print(login('bobby','bobbster')) #False
 print(login('bobby','bobster')) #True
 '''
 def view_one(story):
+    DB_FILE="data/discoeggs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
 
     command = "SELECT content FROM stories WHERE story_name = \'{}\'".format(story)
     c.execute(command)
     contents = c.fetchall()
+
+    db.commit() #save changes
+    db.close()  #close database
+
     #Get last element in array and then last elemtnt in the list that lies inside the array
     #print(contents[-1][-1])
     return (contents[-1][-1])
@@ -43,6 +69,10 @@ def view_one(story):
 
 
 def view_all(id):
+    DB_FILE="data/discoeggs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+
     #stores where the editor has added to the stories
     command = "SELECT story_name FROM stories WHERE editor_id = {}".format(id)
     c.execute(command)
@@ -60,6 +90,7 @@ def view_all(id):
         c.execute(command)
         #Refers to content with tuple and list outside
         uneditedContent = c.fetchall()
+
         content = []
 
         for each in uneditedContent:
@@ -70,19 +101,27 @@ def view_all(id):
         #Getting the insides of the wholeContent
         ret[story[0]] = content #creates a new key for a story with all its content
 
+    db.commit() #save changes
+    db.close()  #close database
+
     return ret
 
 
-view_all(1)
+#view_all(1)
 
 
 def get_id(user):
+    DB_FILE="data/discoeggs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+
     command = "SELECT editor_id FROM login WHERE username = \'{}\' ".format(user)
     #print(command)
     c.execute(command)
     id = c.fetchone()
     #print(id[-1])
-    return(id[-1])
 
-db.commit() #save changes
-db.close()  #close database
+    db.commit() #save changes
+    db.close()  #close database
+
+    return(id[-1])
